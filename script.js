b86e32e8088625d79d291bfcc28ef7d7c4bc9660
AxoloteDev <axolote.dev@gmail.com>
2023-11-19 01:26:00 -0300

    Foi criado a lógica da vitória jogo, isso é feito através da verificação da posição de cada elemento na função isWin() junto da função checkLine() que verifica em qual linha e coluna estes elementos estão, já para o empate do jogo é feito verificando se o tabuleiro está cheio ou não por um laço for na função isBoardFull().

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = document.querySelector('#gameBoard');
+const gameBoard = document.querySelector("#gameBoard");
 let cells = gameBoard.querySelectorAll(".cell");
 
 let isPlayerOneTurn = true;
@@ -22,5 +22,40 @@ cells.forEach((cell)=>{
 
             isPlayerOneTurn = true;
         }
+        if (isWin()) {
+            alert(isPlayerOneTurn ? "O ganhou!" : "X ganhou!");
+        } else if (isBoardFull()) {
+            alert("Empate!");
+        }
     });
 });
+
+function isWin(){
+    return (
+        checkLine(0, 1, 2) ||
+        checkLine(3, 4, 5) ||
+        checkLine(6, 7, 8) ||
+        checkLine(0, 3, 6) ||
+        checkLine(1, 4, 7) ||
+        checkLine(2, 5, 8) ||
+        checkLine(0, 4, 8) ||
+        checkLine(2, 4, 6)
+    );
+}
+
+function checkLine(a, b, c) {
+    return (
+        cells[a].innerHTML !== '' &&
+        cells[a].innerHTML === cells[b].innerHTML &&
+        cells[a].innerHTML === cells[c].innerHTML
+    );
+}
+
+function isBoardFull(){
+    for(let i = 0; i < cells.length; i++){
+        if(cells[i].innerHTML === ''){
+            return false;
+        }
+    }
+    return true;
+}
