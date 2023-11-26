8af2bc832e144a8da9378f0081d112c840cdd448
AxoloteDev <axolote.dev@gmail.com>
2023-11-26 20:58:09 -0300

    Foi adicionado um indicador de turno, além disso, agora quando algum jogador ganha, um modal exibindo a mensagem 'player 1 ou 2 Win'. futuramente será adicionado um botão de reiniciar partida.

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
+const body = document.querySelector('body');
+const turn = document.querySelector('#player-turn');
+const winScreen = document.querySelector('#win-screen');
+const playerWin = document.querySelector('#player-win');
 const gameBoard = document.querySelector("#gameBoard");
 let cells = gameBoard.querySelectorAll(".cell");
 
 let isPlayerOneTurn = true;
+turn.innerHTML = 'Player 1 Turn';
+body.style.backgroundColor = "#483d8b";
 
 cells.forEach((cell)=>{
     cell.addEventListener('click', ()=>{
         if(isPlayerOneTurn === true){
             if(cell.innerHTML === ''){
                 cell.innerHTML = "X";
+                body.style.backgroundColor = "#940c27";
+                turn.innerHTML = 'Player 2 Turn';
             } else{
                 return null;
             }
-
             isPlayerOneTurn = false;
         } else{
             if(cell.innerHTML === ''){
                 cell.innerHTML = "O";
+                body.style.backgroundColor = "#483d8b";
             } else{
                 return null;
             }
-
             isPlayerOneTurn = true;
+            turn.innerHTML = 'Player 1 Turn';
         }
+        
         if (isWin()) {
-            alert(isPlayerOneTurn ? "O ganhou!" : "X ganhou!");
+            winScreen.style.display = 'flex';
+            isPlayerOneTurn ? winScreen.style.backgroundColor = '#df1c43'
+             : winScreen.style.backgroundColor = '#5b44f1';
+
+            isPlayerOneTurn ? playerWin.innerHTML = "Player 2 Win" : playerWin.innerHTML = "Player 1 win"; 
+
+            isPlayerOneTurn ?  body.style.backgroundColor = "#940c27" :  body.style.backgroundColor = "#483d8b";
         } else if (isBoardFull()) {
-            alert("Empate!");
+            alert('Empate!');
         }
     });
 });
